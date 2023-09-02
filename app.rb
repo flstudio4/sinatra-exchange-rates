2b73994e40e8427b45f525d384033139ee2cfb4c
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-09-02 21:10:53 +0000

    final changes

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 require "sinatra"
 require "sinatra/reloader"
+require "net/http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  url = URI("https://api.exchangerate.host/symbols")
+  raw_data = Net::HTTP.get(url)
+  parsed_data = JSON.parse(raw_data)
+  @hash = parsed_data.fetch("symbols")
+  erb(:app)
+end
+
+get("/:from") do
+  @symbol1 = params.fetch("from")
+  url = URI("https://api.exchangerate.host/symbols")
+  raw_data = Net::HTTP.get(url)
+  parsed_data = JSON.parse(raw_data)
+  @hash = parsed_data.fetch("symbols")
+  erb(:from)
+end
+
+get("/:from/:to") do
+  @symbol1 = params.fetch("from")
+  @symbol2 = params.fetch("to")
+  url = URI("https://api.exchangerate.host/convert?from=#{@symbol1}&to=#{@symbol2}")
+  raw_data = Net::HTTP.get(url)
+  parsed_data = JSON.parse(raw_data)
+  @hash = parsed_data.fetch("info")
+  @value = @hash.fetch("rate")
+  erb(:to)
 end
